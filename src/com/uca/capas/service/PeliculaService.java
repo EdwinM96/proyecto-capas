d07a760a3c43da16d3c98d0bb1cd4cc0d875d4a7
EdwinM96 <edwin.morales9b@gmail.com>
2019-06-14 13:23:53 -0600

    Added PeliculaService

---
+++ src/com/uca/capas/service/PeliculaService.java
@@ -0,0 +1,18 @@
+package com.uca.capas.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.uca.capas.dao.PeliculaDAO;
+import com.uca.capas.domain.Pelicula;
+
+public class PeliculaService {
+
+	@Autowired
+	PeliculaDAO peliculaDao;
+	
+	public List<Pelicula> findAll(){
+		return peliculaDao.listAll();
+	}
+}
