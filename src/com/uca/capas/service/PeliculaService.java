262f647af3836958875b60af62dceaf153d38722
EdwinM96 <edwin.morales9b@gmail.com>
2019-06-14 13:48:58 -0600

    Added the @Service tag to the service classes that had it missing

--- src/com/uca/capas/service/PeliculaService.java
+++ src/com/uca/capas/service/PeliculaService.java
@@ -3,10 +3,12 @@ package com.uca.capas.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.uca.capas.dao.PeliculaDAO;
 import com.uca.capas.domain.Pelicula;
 
+@Service
 public class PeliculaService {
 
 	@Autowired
